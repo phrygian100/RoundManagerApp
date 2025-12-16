8ebd4ce3839db96411137ed6d149ab61b353be92
Travis Muller <travis_gm@llive.co.uk>
2025-12-16 18:36:44 +0000

    Pull Additional Services section higher up to prevent content being cut off by footer

--- app/materials.tsx
+++ app/materials.tsx
@@ -3491,7 +3491,7 @@ const flyerStyles = StyleSheet.create({
     backgroundColor: 'rgba(255,255,255,0.4)',
     padding: 10,
     position: 'relative',
-    marginTop: -80,
+    marginTop: -120,
   },
   additionalTitle: {
     fontSize: 14,
